08e5decee490608636d47a0fa7ad68273af6017f
xento-app <49149038+xento-app@users.noreply.github.com>
2019-04-01 14:26:41 +0200

    Update ExceptionInterface.php

--- src/Exception/ExceptionInterface.php
+++ src/Exception/ExceptionInterface.php
@@ -3,7 +3,7 @@
  * @name ExceptionInterface
  * @version 1.0.0
  * @author Miroslaw Kukuryka
- * @copyright (c) 2018 (https://www.xento.app)
+ * @copyright (c) 2019 (https://www.xento.app)
  * @link https://www.xento.app
  */
 namespace Xento\Exception;
