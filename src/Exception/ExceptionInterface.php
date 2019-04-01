e2e768ec458647be855517d755999990ef2c6020
xento-app <49149038+xento-app@users.noreply.github.com>
2019-04-01 12:59:18 +0200

    Create ExceptionInterface.php
    Create file ExceptionInterface.php

---
+++ src/Exception/ExceptionInterface.php
@@ -0,0 +1,13 @@
+<?php
+/**
+ * @name ExceptionInterface
+ * @version 1.0.0
+ * @author Miroslaw Kukuryka
+ * @copyright (c) 2018 (https://www.xento.app)
+ * @link https://www.xento.app
+ */
+namespace Xento\Exception;
+
+interface ExceptionInterface
+{
+} 
