d0adaaf29e20b67312d1f40d2dc7e174bc7c8ab6
xento-app <49149038+xento-app@users.noreply.github.com>
2019-04-01 14:01:31 +0200

    Create AppCore.php
    Create new fiel AppCore.php

---
+++ src/AppCore.php
@@ -0,0 +1,13 @@
+<?php
+/**
+ * @name AppCore
+ * @version 1.0.0
+ * @author Miroslaw Kukuryka
+ * @copyright (c) 2018 (https://www.appsonline.eu)
+ * @link https://www.appsonline.eu
+ */
+namespace Xento;
+
+class AppCore
+{
+}
