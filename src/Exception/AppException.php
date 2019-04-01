350ba309a245a71bc379688dd500f25782521231
xento-app <49149038+xento-app@users.noreply.github.com>
2019-04-01 13:01:52 +0200

    Creeate AppException.php
    Create new file AppException.php

---
+++ src/Exception/AppException.php
@@ -0,0 +1,13 @@
+<?php
+/**
+ * @name AppException
+ * @version 1.0.0
+ * @author Miroslaw Kukuryka
+ * @copyright (c) 2018 (https://www.xento.app)
+ * @link https://www.xento.app
+ */
+namespace Xento\Exception;
+
+class AppException extends \Exception implements ExceptionInterface
+{
+}
